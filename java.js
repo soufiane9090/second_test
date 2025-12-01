4a8b4fb9763c59a145bc4d161c9f394a2466fbb3
hamza <amalmoradif0@gmail.com>
2025-12-01 21:25:46 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+
+document.getElementById("number").addEventListener("input" , ()=>{
+    const number = document.getElementById("number").value;
+    document.getElementById("resultat").innerText = number;
+
+})
